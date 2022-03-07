a9ff2ec652cf6c50dbc85ecaaa5a5c351d4e991f
Madeleine <mahdieh.moghadasi@gmail.com>
2022-03-07 18:58:33 +0000

    connect db

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,15 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
+
+try {
+  db.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 module.exports = db;
