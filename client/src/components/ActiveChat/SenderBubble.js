f1742ddba950fa887ab1d694019d03c0bed24559
Madeleine <mahdieh.moghadasi@gmail.com>
2022-03-16 19:06:38 +0000

    pr review

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Box, Typography } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Typography, Avatar } from "@material-ui/core";
+import OtherUserBubble from "./OtherUserBubble";
 
 const useStyles = makeStyles(() => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-end',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end",
   },
   date: {
     fontSize: 11,
-    color: '#BECCE2',
-    fontWeight: 'bold',
+    color: "#BECCE2",
+    fontWeight: "bold",
     marginBottom: 5,
   },
   text: {
     fontSize: 14,
-    color: '#91A3C0',
+    color: "#91A3C0",
     letterSpacing: -0.2,
     padding: 8,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   bubble: {
-    background: '#F4F6FA',
-    borderRadius: '10px 10px 0 10px',
+    background: "#F4F6FA",
+    borderRadius: "10px 10px 0 10px",
+  },
+  avatar: {
+    height: 30,
+    width: 30,
+    marginLeft: 11,
+    marginTop: 6,
   },
 }));
 
-const SenderBubble = ({ time, text }) => {
+const SenderBubble = ({ time, text, lastMessage, otherUser }) => {
   const classes = useStyles();
 
   return (
@@ -36,8 +43,16 @@ const SenderBubble = ({ time, text }) => {
       <Box className={classes.bubble}>
         <Typography className={classes.text}>{text}</Typography>
       </Box>
+      {lastMessage && (
+        <Avatar
+          src={otherUser.photoUrl}
+          alt={otherUser.username}
+          className={classes.avatar}
+        />
+      )}
     </Box>
   );
 };
 
 export default SenderBubble;
+
