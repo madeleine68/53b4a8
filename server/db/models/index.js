354cfdeba8c2c5c563bec24219ff1de119891221
Madeleine <mahdieh.moghadasi@gmail.com>
2022-03-18 15:38:25 -0400

    redesign db

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,19 +1,21 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Group = require('./group');
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, { through: Group});
+Conversation.belongsToMany(User, { through: Group});
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 Conversation.hasMany(Message, { as: "readMessages" });
 Conversation.hasMany(Message, { as: "unreadMessages" });
 
+
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  Group
 };
